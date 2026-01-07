c16b6d1a47e4950739489595c158f8ac4737aaa1
kyhirsch-lang <kyhirsch@gmail.com>
2026-01-06 18:34:49 -0700

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Everywhere I Go!',
+  description: 'Remember what you ordered at every restaurant',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
